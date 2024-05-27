45ae54f45e347ac32e429d805fbf16ccf98a1f41
SrinathhSatuluri <157357363+SrinathhSatuluri@users.noreply.github.com>
2024-05-26 19:00:19 -0500

    This is the girst commit.

---
+++ Finance-Risk-Managment-System/src/main/java/com/Personal/Project/Finance/Risk/Managment/System/FinanceRiskManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Personal.Project.Finance.Risk.Managment.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceRiskManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceRiskManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ Finance-Risk-Managment-System/src/test/java/com/Personal/Project/Finance/Risk/Managment/System/FinanceRiskManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Personal.Project.Finance.Risk.Managment.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceRiskManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
